28d3376b7e520e6f9b822f5ae75cfe6e7a6967bc
MalPrasad <72528382+MalPrasad@users.noreply.github.com>
2020-11-19 19:08:13 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+
+// Your code goes here
+
+// Exercise: FUNCTIONS
+
+// Exercise: ARGUMENTS
+
+// Passing Variables as arguments when we invoke the function
+
+// Exercise: CONTROL FLOW
+
+// ADDING LOGIC(code available on slides)
+
+// Exercise: FUNCTION LOGIC
+
+
+// SCOPE (code available on slides)
